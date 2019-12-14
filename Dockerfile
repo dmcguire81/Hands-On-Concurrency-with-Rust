79f81a5cd73c88e7fe7ff216a4d91ef120c890b9
David McGuire <david.mcguire@joylabs.com>
2019-12-14 02:56:54 +0000

    Downgrade to stretch to fix linux-perf; install afl & criterion deps

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM rust:1.39
+FROM rust:1.39-stretch
 
 COPY ./setup.sh /tmp
 
--- setup.sh
+++ setup.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y lldb gdb valgrind gcc-arm-linux-gnueabihf
+# Chapter 1
 
+apt-get update
+apt-get install -y lldb gdb valgrind
+
+# https://www.raspberrypi.org/products/raspberry-pi-3-model-b-plus/
+apt-get install -y gcc-arm-linux-gnueabihf
 rustup target add armv7-unknown-linux-gnueabihf
 
 cat << CONFIG > ${CARGO_HOME}/config
 [target.armv7-unknown-linux-gnueabihf]
 linker = "arm-linux-gnueabihf-gcc"
 CONFIG
+
+# Chapter 2
+
+# https://github.com/rust-fuzz/afl.rs
+cargo install afl
+
+# https://github.com/bheisler/criterion.rs#quickstart
+apt-get install -y gnuplot
+
+apt-get install -y linux-perf
