cfb9a6bd6479999fcf715f4791a04bee48ecde0b
David McGuire <david.mcguire@joylabs.com>
2020-01-02 23:29:23 +0000

    Fix errors and warnings

--- Chapter09/zero_count/src/lib.rs
+++ Chapter09/zero_count/src/lib.rs
@@ -1,10 +1,12 @@
+/// # Safety
+/// 
+/// This function needs to be supplied with an arr parameter that is at least as long
+/// as the len supplied, lest a buffer overrun will result
 #[no_mangle]
-pub extern "C" fn tail_zero_count(arr: *const u16, len: usize) -> u64 {
+pub unsafe extern "C" fn tail_zero_count(arr: *const u16, len: usize) -> u64 {
     let mut zeros: u64 = 0;
-    unsafe {
-        for i in 0..len {
-            zeros += (*arr.offset(i as isize)).trailing_zeros() as u64
-        }
+    for i in 0..len {
+        zeros += (*arr.offset(i as isize)).trailing_zeros() as u64
     }
     zeros
 }
