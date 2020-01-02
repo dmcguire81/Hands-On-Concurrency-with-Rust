f93fdcfe31bfd885594b98d9b6b0232b10143f0e
David McGuire <david.mcguire@joylabs.com>
2020-01-02 23:25:07 +0000

    Fix errors and warnings

--- Chapter09/embed_quantiles/src/lib.rs
+++ Chapter09/embed_quantiles/src/lib.rs
@@ -8,27 +8,29 @@ pub extern "C" fn alloc_ckms(error: f64) -> *mut CKMS<f32> {
     Box::into_raw(ckms)
 }
 
+/// # Safety
+///
+/// This function should only be called on a struct allocated with alloc_ckms, and only once.
 #[no_mangle]
-pub extern "C" fn free_ckms(ckms: *mut CKMS<f32>) -> () {
-    unsafe {
-        let ckms = Box::from_raw(ckms);
-        drop(ckms);
-    }
+pub unsafe extern "C" fn free_ckms(ckms: *mut CKMS<f32>) {
+    let ckms = Box::from_raw(ckms);
+    drop(ckms);
 }
 
 #[no_mangle]
-pub extern "C" fn ckms_insert(ckms: &mut CKMS<f32>, value: f32) -> () {
+pub extern "C" fn ckms_insert(ckms: &mut CKMS<f32>, value: f32) {
     ckms.insert(value)
 }
 
+/// # Safety
+///
+/// This function should only be called on a struct allocated with alloc_ckms.
 #[no_mangle]
-pub extern "C" fn query(ckms: &mut CKMS<f32>, q: f64, quant: *mut f32) -> i8 {
-    unsafe {
-        if let Some((_, res)) = ckms.query(q) {
-            *quant = res;
-            0
-        } else {
-            -1
-        }
+pub unsafe extern "C" fn query(ckms: &mut CKMS<f32>, q: f64, quant: *mut f32) -> i8 {
+    if let Some((_, res)) = ckms.query(q) {
+        *quant = res;
+        0
+    } else {
+        -1
     }
 }
